75159526013c9d195c51d8e56c66e28e5c329450
jovimc <jovimoct2020@gmail.com>
2026-06-21 05:55:34 +0900

    calendario

---
+++ src/calendario.py
@@ -0,0 +1,10 @@
+def lista_de_calendarios(jogos):
+    '''mostra todas as partidas realizadas e as proximas'''
+    print("\n==========================================") 
+    print("          LISTA DE CALENDÁRIOS           ")
+    print("==========================================")
+    for jogo in jogos:
+        gols1 = jogo["gols1"] if jogo["gols1"] != -1 else ""
+        gols2 = jogo["gols2"] if jogo["gols2"] != -1 else ""
+        print(f"ID: {jogo['ID']} - Fase: {jogo['fase']} - Grupo: {jogo['grupo']} ")
+        print(f"\n  {jogo['time1']} {gols1} X {gols2} {jogo['time2']}\n")
