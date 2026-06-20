351908b37afeb4566bc2caa58272c73b9b6f8b5f
levyve <levimorais4@gmail.com>
2026-06-20 16:36:58 -0300

    BugFix

--- src/apostador.py
+++ src/apostador.py
@@ -1,6 +1,11 @@
 def CadastrarApostador(nomearquivo):
-    with open(nomearquivo, 'a', encoding='utf-8') as arq:
+    with open(nomearquivo, 'a+', encoding='utf-8') as arq: ## a+ é leitura e escrita sem apagar, nao substitui o texto, adiciona
         nome = input("Insira o nome do novo apostador: ")
-        arq.write(nome + '\n')
+        arq.seek(0) # o a+ faz o arquivo iniciar no final, isso aqui faz ele voltar pro inicio
+        if nome.lower() in [linha.strip().lower() for linha in arq]: #Verifica se o nome é repetido
+            return print("Erro: Apostador já cadastrado!")
+        arq.write('\n'+nome )
+        
+
 
 CadastrarApostador('Archives/apostadores.txt')
