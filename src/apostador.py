2eed86c0fa74383e10c7e639994e198bceae353b
ppiettro <joaoaleixo2816@gmail.com>
2026-06-20 17:05:51 -0300

    Adição e teste da função que gera um arquivo JSON de palpites do jogador cadastro

--- src/apostador.py
+++ src/apostador.py
@@ -1,11 +1,34 @@
-def CadastrarApostador(nomearquivo):
+import json
+
+def CadastrarApostador(jogos: list, nomearquivo: str = "apostadores.txt"):
+    """Cadastro um novo apostador
+
+    :param nomearquivo: nome do arquivo que conterá os apostadores
+    :type nomearquivo: str
+
+    :param jogos: uma lista de jogos contendo cada partida.
+    :type jogos: list
+    """
+
     with open(nomearquivo, 'a+', encoding='utf-8') as arq: ## a+ é leitura e escrita sem apagar, nao substitui o texto, adiciona
         nome = input("Insira o nome do novo apostador: ")
         arq.seek(0) # o a+ faz o arquivo iniciar no final, isso aqui faz ele voltar pro inicio
         if nome.lower() in [linha.strip().lower() for linha in arq]: #Verifica se o nome é repetido
             return print("Erro: Apostador já cadastrado!")
         arq.write('\n'+nome )
-        
+
+        CriarArquivoPalpite(jogos, nome)
 
 
-CadastrarApostador('Archives/apostadores.txt')
+def CriarArquivoPalpite(jogos: list, apostador: str):
+    """Cria um arquivo de palpites pera um apostador
+
+    :param jogos: uma lista de jogos contendo cada partida.
+    :type jogos: list
+
+    :param apostador: nome do apostador
+    :type apostador: str
+    """
+
+    with open(f"Archives/palpite_{apostador}.json", "w", encoding="utf-8") as arq:
+        json.dump(jogos, arq, indent=4, ensure_ascii=False)
