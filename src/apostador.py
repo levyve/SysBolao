d383bd25c08ae94f7e2f09bfbe7dc1c90bc3e7ba
ppiettro <joaoaleixo2816@gmail.com>
2026-06-21 16:46:55 -0300

    Bug Fix

--- src/apostador.py
+++ src/apostador.py
@@ -16,7 +16,7 @@ def Atualizar_Arquivo_Palpites(jogos: list, apostador: str):
         json.dump(jogos, arq, indent=4, ensure_ascii=False)
 
 
-def Atualizar_Palpites(apostador: str, jogos: list, id: str, gols1: str, gols2: str):
+def Atualizar_Palpites(apostador: str, jogos: list, jogo: dict, id: str, gols1: str, gols2: str):
     """Atualiza o palpite de um dados apostador em um jogo específico.
 
     :param apostador: nome do apostador.
@@ -24,6 +24,9 @@ def Atualizar_Palpites(apostador: str, jogos: list, id: str, gols1: str, gols2: 
     
     :param jogos: lista contendo todos os jogos, com ou sem palpites, do apostdor.
     :type jogos: list
+    
+    :param jogo: dicionário que representa o jogo.
+    :type jogo: dict
     
     :param id: ID do jogo que o apostador deseja alterar.
     :type id: str
@@ -34,22 +37,20 @@ def Atualizar_Palpites(apostador: str, jogos: list, id: str, gols1: str, gols2: 
     :param gols2: número de gols da segunda seleção.
     :type gols2: str
     """
-    
-    for jogo in jogos:
-        if(jogo['id'] == int(id)):
-            partida = {
-            "id": int(id),
-            "fase": 1,
-            "grupo": jogo['grupo'],
-            "selecao1": jogo['selecao1'],
-            "selecao2": jogo['selecao2'],
-            "gols1":int(gols1),
-            "gols2": int(gols2),
-            }
 
-            jogos.insert(jogos.index(jogo), partida)
-            jogos.pop(jogos.index(jogo))
-    
+    partida = {
+    "id": int(id),
+    "fase": 1,
+    "grupo": jogo['grupo'],
+    "selecao1": jogo['selecao1'],
+    "selecao2": jogo['selecao2'],
+    "gols1":int(gols1),
+    "gols2": int(gols2),
+    }
+
+    jogos.insert(jogos.index(jogo), partida)
+    jogos.pop(jogos.index(jogo))
+
     Atualizar_Arquivo_Palpites(jogos, apostador)
 
 
@@ -72,9 +73,6 @@ def Validar_Jogador(nome: str):
 
 def Cadastrar_Apostador(jogos: list):
     """Cadastra um novo apostador
-
-    :param nomearquivo: nome do arquivo que conterá os apostadores
-    :type nomearquivo: str
 
     :param jogos: uma lista de jogos contendo cada partida.
     :type jogos: list
@@ -314,23 +312,26 @@ def Cadastrar_Palpites(modo: str):
 def Completar_Palpites_Aleatoriamente(apostador: str, jogos: list):
     
     qtd_preenchidos = 0
+    qtd_gols = [0, 1, 2, 3, 4, 5, 6, 7]
+    pesos = [60, 50, 40, 30, 25, 20, 15, 10]
+
     for jogo in jogos:
         if (jogo['gols1'] == -1 or jogo['gols2'] == -1):
-            gols1 = random.randint(0, 10)
-            gols2 = random.randint(0, 10)
+            gols1 = random.choices(qtd_gols, weights=pesos, k=1)[0]
+            gols2 = random.choices(qtd_gols, weights=pesos, k=1)[0]
+
 
             if (jogo['fase'] != 1):  # fases eliminatórias não permitem empate
                 while (gols1 == gols2):
-                    gols1 = random.randint(0, 10)
-                    gols2 = random.randint(0, 10)
+                    gols1 = random.choices(qtd_gols, weights=pesos, k=1)[0]
+                    gols2 = random.choices(qtd_gols, weights=pesos, k=1)[0]
 
-            jogo['gols1'] = gols1
-            jogo['gols2'] = gols2
+
+            Atualizar_Palpites(apostador, jogos, jogo, str(jogo['id']), gols1, gols2)
             qtd_preenchidos += 1
-
-    Atualizar_Arquivo_Palpites(jogos, apostador)
 
     print(f"\n{qtd_preenchidos} palpite(s) pendente(s) preenchido(s) aleatoriamente para {apostador}!")
 
 
 Completar_Palpites_Aleatoriamente('levy', jogos=(Gerar_Primeira_Fase(Carregar_Selecoes('Archives/txt/selecoes.txt'))))
+# Cadastrar_Apostador(jogos=Gerar_Primeira_Fase(Carregar_Selecoes('Archives/txt/selecoes.txt')))
