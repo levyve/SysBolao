19dd29db7c9586851a30138052c8ba85f8f75b79
jovimc <jovimoct2020@gmail.com>
2026-06-21 23:09:18 +0900

    jogos por grupo atualizada

--- src/jogos_por_grupo.py
+++ src/jogos_por_grupo.py
@@ -1,8 +1,9 @@
-def jogos_por_grupo(jogos, grupo_escolhido):
+def jogos_por_grupo(jogos):
     '''função recebe os grupo escolhido no menu e demonstra os jogos e placares desse grupo
     jogos: arquivo contendo a lista de dicionarios com todos os dados sobre os jogos(id, placar, times, etc.)
     grupo_escolhido: variavel escolhida pelo usuario para acessar o grupo em jogos
     '''
+    grupo_escolhido = input("digite o grupo que você pretende checar: ").upper()
     print(f"           Grupo: {grupo_escolhido}    ")
     print("--------------------------------")
     for jogo in jogos:
